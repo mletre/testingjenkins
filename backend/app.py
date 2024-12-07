7be90a842fc8c3db7771c620925ff9bebab3a465
yanuarakhid <yanuarakhid@gmail.com>
2024-12-07 17:57:42 +0700

    update

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Testing CICD ! with Jenkins ☁️☁️☁️"
+    return "Testing CICD !! with Jenkins ☁️☁️☁️"
 
 if __name__ == "__main__":
     app.run(port=9696)
